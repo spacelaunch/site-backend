13bc827ea7840440f95108fdf34eddddd7c737c9
LucaSantopadre <luca.santopadre92@gmail.com>
2020-03-01 13:35:09 +0100

    First commit

---
+++ src/test/java/eu/spacelaunch/sitebackend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.spacelaunch.sitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
